b1b81375941cd6800a0a36b3655aa94c64749bf5
Crowdbotics <team@crowdbotics.com>
2019-03-26 05:10:05 +0000

    initial commit

---
+++ android/app/src/main/java/com/cerralo_crm_1665/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cerralo_crm_1665;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "cerralo_crm_1665";
+    }
+}
